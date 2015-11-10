9dd841b48107d485278076aece6cee94a554bd1e
vincent he <vincent.ucas@gmail.com>
2015-11-10 16:35:02 +0800

    Create Slave.php

---
+++ application/controllers/Slave.php
@@ -0,0 +1,19 @@
+class Hello  extends CI_Controller {
+
+	public function capture($enemy)
+	{
+		
+	}
+
+	public function tansfer($enemy){
+
+	}
+
+	public function free($enemy){
+
+	}
+
+	public function raise(){
+
+	}
+}
