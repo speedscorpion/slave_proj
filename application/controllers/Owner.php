2918d9f45f3feffb3bf7a1b82770bbc07de42b90
yyhc <yyhejun@163.com>
2015-11-11 19:24:26 +0800

    Create Owner.php

---
+++ application/controllers/Owner.php
@@ -0,0 +1,20 @@
+<?php
+class Owner  extends CI_Controller {
+
+    public function capture($enemy)
+    {
+        
+    }
+
+    public function tansfer($enemy){
+
+    }
+
+    public function free($enemy){
+
+    }
+
+    public function raise(){
+
+    }
+}
