a245a9deceaf4c074aa74f503943333d2e916680
panboxiaosa <864633261@qq.com>
2015-11-13 00:35:49 +0800

    test

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,8 +4,8 @@ class User extends CI_Controller{
 
     public function nearby(){
     	$id = get_cookie("slave_game_user_id");
-    	$query = $this->db->query('select id, nickname, state from user where state = 1 and state = 2;');
-    	return $query->result_array();
+    	$query = $this->db->query('select id, nickname, state from user where state = 1 or state = 2 and id != $id;');
+    	return json_encode($query->result_array());
     }
 
     public function show(){
--- application/views/owner/palace.php
+++ application/views/owner/palace.php
@@ -7,24 +7,16 @@
 <body>
 	<h1>in my palace</h1>
 	<hr/>
-	<button id="go_capture">to capture</button>
 	<button id="go_jail">to jail</button>
 	<button id="go_stand">to stand</button>
 	<button id="neigborhood">neigborhood</button>
-	
+	<br/>
+	<hr/>
+	<div id="holder"></div>
+
 	<script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript">
 		$(function() {
-			$("#go_capture").click(function(){
-				$.ajax({
-					type: "get",
-					url: "http://www.hereprovides.com/slave/index.php/User/nearby",
-					dataType: "json",
-					success: function(data){
-
-					}
-				})
-			});
 
 			$("#go_stand").click(function(){
 
@@ -35,7 +27,14 @@
 			});
 
 			$("#neigborhood").click(function(){
-
+				$.ajax({
+					type: "get",
+					url: "http://www.hereprovides.com/slave/index.php/User/nearby",
+					dataType: "json",
+					success: function(data){
+						alert(data);
+					}
+				});
 			});
 		});
 	</script>
