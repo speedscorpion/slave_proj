122a99de7307184665e1e927bf31bb1838895b2d
panboxiaosa <864633261@qq.com>
2015-11-13 01:06:40 +0800

    test

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,7 @@ class User extends CI_Controller{
     public function nearby(){
     	$id = get_cookie("slave_game_user_id");
         $states = '(1, 2)';
-        $statement = 'select * from user where state in (1, 2) and id != '. $id;
+        $statement = 'select * from user where state in (1, 2) and id != \''. $id . '\'';
         $query = $this->db->query($statement);
     	return json_encode($query->result_array());
     }
