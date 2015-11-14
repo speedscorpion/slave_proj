50dbee8e1eef58b82bae2a3d3f9075e9dc092b44
panboxiaosa <864633261@qq.com>
2015-11-14 16:08:49 +0800

    test

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -11,10 +11,26 @@ class User extends CI_Controller{
     }
 
     public function show(){
-    	
+    	$id = get_cookie("slave_game_user_id");
+        $this->load->view('show', $this->get_data($id));
     }
 
-    public function update(){
-
+    private function get_data($id){
+        $data = [];
+        $query = $this->db->query('select * from slave where owner_id = \''.$id.'\';');
+        $data['owner_record'] = $query->result();
+        $query = $this->db->query('select * from slave where slave_id = \''.$id.'\';');
+        $data['slave_record'] = $query->result();
+        $query = $this->db->query('select count(*) from threat where state = 3 and owner_id = \''.$id.'\';');
+        $data['handle_count'] = $query->row()->count(*);
+        $query = $this->db->query('select count(*) from threat where state = 2 and flag in (select flag from raise where slave_id = \''.$id.'\');');
+        $data['raise_count'] = $query->row()->count(*);
+        return $data;
     }
+
+    public function state($player)
+    {
+        $this->load->view('show', $this->get_data($player));
+    }
+
 }
---
+++ application/views/show.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>slave game</title>
+</head>
+<body>
+	<h1><?php echo $nickname. " " ?>in slave game</h1>
+	<hr>
+	<h2>朕的奴隶</h2>
+		<?php
+			foreach ($data->owner_record as $item) {
+				$tag = "<a href='http://www.hereprovides.com/slave/index.php/User/state/".$item->id."'>".$item->nickname."</a><br/>";
+				echo $tag;
+			}
+		?>
+	<hr>
+	<h2>我的主人</h2>
+		<?php
+			foreach ($data->slave_record as $item) {
+				$tag = "<a href='http://www.hereprovides.com/slave/index.php/User/state/".$item->id."'>".$item->nickname."</a><br/>";
+				echo $tag;
+			}
+		?>
+	<hr>
+	<h2>平定叛乱</h2>
+		<?php 
+			echo $data->handle_count. '次';
+		?>
+	<hr>
+	<h2>推翻暴政</h2>
+		<?php 
+			echo $data->raise_count. '次';
+		?>
+	<hr>
+
+	<a href="http://www.hereprovides.com/slave/index.php/Player/enter">join game</a>
+	<script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
