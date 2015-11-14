4e92fadb8461d74ac3dcc626ab0e1022ead0b599
panboxiaosa <864633261@qq.com>
2015-11-14 14:09:26 +0800

    test

--- application/views/owner/jail.php
+++ application/views/owner/jail.php
@@ -7,6 +7,13 @@
 <body>
 	<h1>my prison</h1>
 	<hr>
+		<?php
+			foreach ($data as $item) {
+				$tag = "<button class=\"slave_name\" val=\"". $item->id. "\">". $item->nickname. "</button><br/>";
+				echo $tag;
+			}
+		 ?>
+	<hr>
 	<button id="back_palace">my palace</button>
 	<script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript">
@@ -15,6 +22,9 @@
 				location.href="http://www.hereprovides.com/slave/index.php/Player/enter";
 			});
 
+			$(".slave_name").click(function(){
+				alert('laught at' + $(this).attr(val));
+			});
 		});
 	</script>
 </body>
